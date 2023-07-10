093e8252c761eb6db1a142f62197435313b49502
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-07-11 00:22:33 +0300

    Update mult_control.v

--- mult_control/mult_control.v
+++ mult_control/mult_control.v
@@ -20,25 +20,23 @@ parameter MSB = 3'b011; // Found 110
 parameter CALC_DONE = 3'b100; // Found 1101
 parameter ERR = 3'b101;
 
-// Define the state register
-reg [2:0] state;
-always @(posedge clk, posedge reset_a) begin
-  if (reset_a) begin
-    state <= IDLE;
+always @(posedge clk, negedge reset_a) begin
+  if (~reset_a) begin
+    state_out <= IDLE;
   end
   else begin
-    case (state)
+    case (state_out)
       IDLE: if (start) begin 
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b0;
-                state <= LSB;
+                state_out <= LSB;
             end
             else begin
                 done <= 1'b0;
                 clk_ena <= 1'b0;
                 sclr_n <= 1'b1;
-                state <= IDLE;
+                state_out <= IDLE;
             end
       LSB: if (~start && count == 2'b00) begin 
                 input_sel <= 2'b00;
@@ -46,13 +44,13 @@ always @(posedge clk, posedge reset_a) begin
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b1;
-                state <= MID;
+                state_out <= MID;
             end
             else begin
                 done <= 1'b0;
                 clk_ena <= 1'b0;
                 sclr_n <= 1'b1;
-                state <= ERR;
+                state_out <= ERR;
             end
       MID: if (~start && count == 2'b10) begin 
                 input_sel <= 2'b10;
@@ -60,7 +58,7 @@ always @(posedge clk, posedge reset_a) begin
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b1;
-                state <= MSB;
+                state_out <= MSB;
             end
             else if (~start && count == 2'b01) begin 
                 input_sel <= 2'b01;
@@ -68,13 +66,13 @@ always @(posedge clk, posedge reset_a) begin
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b1;
-                state <= MID;
+                state_out <= MID;
             end
             else begin
                 done <= 1'b0;
                 clk_ena <= 1'b0;
                 sclr_n <= 1'b1;
-                state <= ERR;
+                state_out <= ERR;
             end
       MSB: if (~start && count == 2'b11) begin 
                 input_sel <= 2'b11;
@@ -82,38 +80,39 @@ always @(posedge clk, posedge reset_a) begin
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b1;
-                state <= CALC_DONE;
+                state_out <= CALC_DONE;
             end
             else begin
                 done <= 1'b0;
                 clk_ena <= 1'b0;
                 sclr_n <= 1'b1;
-                state <= ERR;
+                state_out <= ERR;
             end
       CALC_DONE: if (start) begin 
                     done <= 1'b0;
                     clk_ena <= 1'b0;
                     sclr_n <= 1'b1;
-                    state <= ERR;
+                    state_out <= ERR;
                 end
                 else begin
                     done <= 1'b1;
                     clk_ena <= 1'b0;
                     sclr_n <= 1'b1;
-                    state <= IDLE;
+                    state_out <= IDLE;
                 end
       ERR: if (start) begin 
                 done <= 1'b0;
                 clk_ena <= 1'b1;
                 sclr_n <= 1'b0;
-                state <= LSB;
+                state_out <= LSB;
             end
             else begin
                 done <= 1'b0;
                 clk_ena <= 1'b0;
                 sclr_n <= 1'b1;
-                state <= ERR;
+                state_out <= ERR;
             end
+      default: state_out <= IDLE;
     endcase
   end
 end
